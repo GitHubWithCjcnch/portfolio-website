4b2909e40445724be8fed3ead4b76c7fee0265cd
cjcnch <githubwithcjcnch@gmail.com>
2023-01-03 17:35:35 -0300

    finishing the landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -18,13 +18,77 @@ const Landing = ({ setSelectedPage }) => {
           >
             <img 
               alt="profile"
-              src="assets/profile-image.png"
+              className="hover:saturate-200 transition duration-500 z-10 w-full
+                max-w-[400px] md:max-w-[600px]"
+              src="assets/profile-image.jpg"
             />
           </div>
         ) : (
-          <div></div>
+          <img 
+            alt="profile"
+            className="hover:saturate-200 transition duration-500 z-10 w-full
+              max-w-[400px] md:max-w-[600px]"
+            src="assets/profile-image.jpg"
+          />
         )}
       </div>
+
+        <div className="z-30 basis-2/5 mt-12 md:mt-32">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 }
+            }}
+          >
+            <p className="text-6xl font-playfair z-10 text-center md:text-start">
+              Caio {""}
+              <span className="xs: relative xs:text-deep-blue xs:font-semibold z-20
+                xs:before:content-brush before:absolute before:-left-[25px] before:-top-[70px]
+                before:z-[-1]">
+                Vitor
+              </span>
+            </p>
+
+            <p className="mt-10 mb-7 text-sm text-center md:text-start">
+              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </p>
+          </motion.div>
+
+          <motion.div
+            className="flex mt-5 justify-center md:justify-start"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 }
+            }}
+          >
+            <AnchorLink
+              className="bg-gradient-rainblue text-deep-blue py-3 px-7 font-semibold
+                hover:bg-blue hover:text-white transition duration-500"
+              onClick={() => setSelectedPage("contact")}
+              href="#contact"
+            >
+              Contact Me
+            </AnchorLink>
+            <AnchorLink
+              className="bg-gradient-rainblue py-0.5 pr-0.5"
+              onClick={() => setSelectedPage("contact")}
+              href="#contact"
+            >
+              <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center
+                justify-center font-playfair px-10">
+                Vamos conversar
+              </div>
+            </AnchorLink>
+          </motion.div>
+        </div>
     </section>
   );
 };
